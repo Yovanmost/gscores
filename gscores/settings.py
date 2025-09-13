541566f6b98c684d4d32ef0b07782c645c08d69a
Yovanmost <yovanmost@gmail.com>
2025-09-14 00:38:49 +0700

    fix: env and database setup

--- gscores/settings.py
+++ gscores/settings.py
@@ -87,14 +87,7 @@ WSGI_APPLICATION = 'gscores.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'), # default Postgres port
-    }
+    'default': env.db()
 }
 
 
