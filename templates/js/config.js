94a2b82b28339d61e8eb75722e9822e461dbcd74
Yovanmost <yovanmost@gmail.com>
2025-09-14 19:30:32 +0700

    feat: add library

--- templates/js/config.js
+++ templates/js/config.js
@@ -1,1 +1,5 @@
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// this is stupid, dont try this. I only do this because I just want to use simple 
+// html + css + js for frontend, not react/vue/angular
+// and I dont want to setup a backend server to serve the frontend
+// so I have to hardcode the backend api url here
+const API_BASE_URL = "https://gscores-ejhv.onrender.com/api";
